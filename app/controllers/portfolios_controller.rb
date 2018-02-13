9822ba972e92e9330269d8d80083102045e142c8
Pablo R. Hernandez <prh90@yahoo.com>
2018-02-12 19:47:58 -0800

    Adds ability to edit existing item, also adds link to item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,7 +19,28 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
   # private
+
+  #   def set_blog
+  #     @portfolio_item = Portfolio.find(params[:id])
+  #   end
 
   # def portfolio_params
   #   params.require(:portfolio).permit(:title, :subtitle, :body)
