5e7fe38973d1722d73a14f1b34d76ef0c0e7d389
Pablo R. Hernandez <prh90@yahoo.com>
2018-02-16 19:06:28 -0800

    Adds migration file for draft/published status adds enum in model file

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  enum status: { draft: 0, published: 1}
   extend FriendlyId
   friendly_id :title, use: :slugged
   # What this does is that it swaps in the title in for whats in place now.
