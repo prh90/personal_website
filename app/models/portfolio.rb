696097bdd097693b2bbd812d29e0c05745c9aa2e
Pablo R. Hernandez <prh90@yahoo.com>
2018-02-27 20:47:28 -0800

    Adds method to set default image for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,11 @@ class Portfolio < ApplicationRecord
 
   # build method with scope name being called
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
