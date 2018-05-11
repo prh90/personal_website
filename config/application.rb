1668da0fccc064b2650e51fd058ae711494f1765
Pablo R. Hernandez <prh90@yahoo.com>
2018-05-10 20:28:02 -0700

    Adds implementation of gem to connect to twitter

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module PabloPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.eager_load_paths << "#{Rails.root}/lib"
 
     # Allows all get permitted, not really good. Overrides permit in controller
     # config.action_controller.permit_all_parameters = true
