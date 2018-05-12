f11953e1b4e888e2086230dc6f52d3ff7723c6f5
Pablo R. Hernandez <prh90@yahoo.com>
2018-05-11 21:56:14 -0700

    Adds redis gem for comments and added comment generator

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,6 +6,7 @@ class Blog < ApplicationRecord
 
   validates_presence_of :title, :body
   belongs_to :topic, optional: true
+  has_many :comments, dependent: :destroy
 
   def self.by_newest
     order("created_at DESC")
